5706ec4d0c0684e8a96573e2d5cc4ff72c551d1c
Amanda Fernandes <aferanda@gmail.com>
2022-02-08 11:19:48 -0300

    apply formik and yup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Formik
+        initialValues={{ firstname: '', lastname: '', email: '', password: '' }}
+        validationSchema={Yup.object({
+          firstName: Yup
+            .string()
+            .max(15, 'Must be 15 characters or less')
+            .required('First name cannot be empty'),
+          lastName: Yup
+            .string()
+            .max(20, 'Must be 20 characters or less')
+            .required('Last name cannot be empty'),
+          email: Yup
+            .string()
+            .email('Looks like this is not an email')
+            .required('Required'),
+          password: Yup
+            .string()
+            .min(8, 'Must be 8 characters or more')
+            .required('Password cannot be empty')
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field 
+              type='text'
+              name='firstName'
+              placeholder='first name'
+            />
+            <ErrorMessage name='firstName' component='div' />
+            <Field 
+              type='text' 
+              name='lastName'
+              placeholder='last name'
+              />
+            <ErrorMessage name='lastName' component='div' />
+            <Field 
+            type='email' 
+            name='email'
+            placeholder='email'
+            />
+            <ErrorMessage name='email' component='div' />
+            <Field 
+            type='password' 
+            name='password'
+            placeholder='password'
+            />
+            <ErrorMessage name='password' component='div' />
+            <button type='submit' disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
